5f1b065b7825130548bfec789b0916bc6a7137f2
SpKillX <spkillx@yandex.ru>
2025-10-06 23:00:21 +0300

    complite 2.2 task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib.pyplot
 
 print("ВАРИАНТ 11\n\n")
 
@@ -51,3 +51,12 @@ for i in range(n):
 print(list, "\n\n")
 
 
+print("Задание 4 (повышенная сложность) Построить график y=x^2 с matplotlib\n")
+x = []
+y = []
+for i in range(-5, 6):
+    x.append(i)
+    y.append(i**2)
+
+matplotlib.pyplot.plot(x,y)
+matplotlib.pyplot.show()
