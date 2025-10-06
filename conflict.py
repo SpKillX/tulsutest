b94ff0c60d4af7eb1b074849fc4ea458e655ad11
SpKillX <spkillx@yandex.ru>
2025-10-06 22:01:32 +0300

    complite 1 task

--- conflict.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-print('Version from main branch')
-=======
-print('Version from conflict branch')
->>>>>>> conflict-branch
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('Hello git')
-print('Hello git again')
+import matplotlib
+
+print("ВАРИАНТ 11\n\n")
+
+
+print("Задание 1 Таблица умножения\n")
+for i in range(1, 11):
+    for j in range(1,11):
+        print(i*j, end="\t")
+    print("\n")
