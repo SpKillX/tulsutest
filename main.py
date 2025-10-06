89e5486f4ffac27b3b658840dbcf3163f09e9c89
SpKillX <spkillx@yandex.ru>
2025-10-06 23:09:09 +0300

    refactoring 1

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ else:
 
 
 print("Задание 5 Список простых чисел до 100\n")
-for numbers in range(2, 101):
+max_number = 100
+for numbers in range(2, max_number+1):
     is_prime = True
     for i in range(2, int(numbers**0.5)+1):
         if numbers % i == 0:
@@ -39,16 +40,16 @@ print("\n\n")
 
 
 print("Задание 1 (повышенная сложность) Сортировка списка пузырьком\n")
-list = [15, 35, 78, 1, 2, 64, 34, 25, 12, 22, 11, 90]
-n = len(list)
-print(list, end="\n")
+numbers_to_sort = [15, 35, 78, 1, 2, 64, 34, 25, 12, 22, 11, 90]
+n = len(numbers_to_sort)
+print(numbers_to_sort, end="\n")
 for i in range(n):
     for j in range(n-1):
-        if list[j] > list[j+1]:
-            buff = list[j]
-            list[j] = list[j+1]
-            list[j+1] = buff
-print(list, "\n\n")
+        if numbers_to_sort[j] > numbers_to_sort[j+1]:
+            buff = numbers_to_sort[j]
+            numbers_to_sort[j] = numbers_to_sort[j+1]
+            numbers_to_sort[j+1] = buff
+print(numbers_to_sort, "\n\n")
 
 
 print("Задание 4 (повышенная сложность) Построить график y=x^2 с matplotlib\n")
