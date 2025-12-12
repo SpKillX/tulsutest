29339b915c8e31f64e8f9a20109e1542e2c1b379
SpKillX <spkillx@yandex.ru>
2025-12-12 17:58:44 +0300

    complite 6lab 1.3

--- 1.3/main.py
+++ 1.3/main.py
@@ -1,43 +1,9 @@
-import sys
-import os
-from PyQt5 import QtWidgets, uic
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-class MyWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('untitled.ui', self)
-        self.equal.clicked.connect(self.calculate)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-    def calculate(self):
-        try:
-            a = float(self.first_field.text())
-            b = float(self.second_field.text())
-            operation = self.operations.currentText()
-
-            if operation == "+":
-                result = a + b
-            elif operation == "-":
-                result = a - b
-            elif operation == "*":
-                result = a * b
-            elif operation == "/":
-                if b != 0:
-                    result = a / b
-                else:
-                    self.result.setText("Ошибка: деление на 0")
-                    return
-            else:
-                result = a + b
-
-            self.result.setText(f"Результат: {result}")
-
-        except ValueError:
-            self.result.setText("Ошибка ввода: введите числа")
-
-
-
-app = QtWidgets.QApplication(sys.argv)
-window = MyWindow()
-window.show()
-sys.exit(app.exec_())
+app.run()
--- 1.5/main.py
+++ 1.5/main.py
@@ -1,14 +1,9 @@
-import sys
-import os
-from PyQt5 import QtWidgets, uic
+from flask import Flask, render_template
 
-class MyWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('untitled.ui', self)
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-app = QtWidgets.QApplication(sys.argv)
-window = MyWindow()
-window.show()
-sys.exit(app.exec_())
+app.run()
